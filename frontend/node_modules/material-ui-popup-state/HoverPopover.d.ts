8647402800b9063f5c0d1793bd20b5f20409f5af
VanSHOE <75690289+VanSHOE@users.noreply.github.com>
2022-03-23 03:12:05 +0530

    Delete frontend/node_modules directory

--- frontend/node_modules/material-ui-popup-state/HoverPopover.d.ts
+++
@@ -1,5 +0,0 @@
-import { PopoverProps } from '@material-ui/core/Popover'
-
-declare const HoverPopover: React.ComponentType<PopoverProps>
-
-export default HoverPopover
--- frontend/node_modules/material-ui-popup-state/es/HoverMenu.d.ts
+++
@@ -1,5 +0,0 @@
-import { MenuProps } from '@material-ui/core/Menu'
-
-declare const HoverMenu: React.ComponentType<MenuProps>
-
-export default HoverMenu
